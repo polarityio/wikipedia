3e65e5b25b9f0acdf1208e249fdd4c37a43919c7
rader <joem@breachintelligence.com>
2017-02-07 15:35:36 -0500

    Wrong paramenter - updated

--- components/block.js
+++ components/block.js
@@ -1,13 +1,13 @@
 polarity.export = PolarityComponent.extend({
     extract: function(){
 
-        if(typeof(this.get("block.data.details.body[2][0]")) == "undefined"){
+        if(typeof(this.get("block.data.details.para")) == "undefined"){
             return "";
         }
 
-        var e = this.get("block.data.details.body[2][0]");
+        var e = this.get("block.data.details.para");
         var shortentedText = e.match(/^.{1,340}[^\.]*/)+".";
 
         return shortentedText;
-    }.property("block.data.details.extract")
+    }.property("block.data.details.para")
 });
