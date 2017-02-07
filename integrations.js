2d9655b2ce3ae15ceefe78e9d1977c68c5af5074
bill <bill@breachintelligence.com>
2017-02-07 16:55:56 -0500

    updated to pre compute the list on the server

--- integrations.js
+++ integrations.js
@@ -70,7 +70,7 @@ var _createJsonErrorObject = function (msg, pointer, httpCode, code, title, meta
 function _lookupEntity(entityObj, options, cb) {
     let uri = 'https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&namespace=0&format=json&search=' + entityObj.value + '&profile=' + options.profile;
     log.debug("Checking to see if the query executes %j", uri );
-	var relatedCount = (_.isNaN(options.relatedCount))? 5 : options.relatedCount;
+	var relatedCount = ( _.isNaN(options.relatedCount) )? 5 : options.relatedCount;
 
     request({
         uri: uri,
@@ -102,7 +102,17 @@ function _lookupEntity(entityObj, options, cb) {
         }
 
         // The lookup results returned is an array of lookup objects with the following format
-        else {   cb(null, {
+        else {   
+		
+			var relatedList = new Array();
+			for(var i = 0; i < body[1].length && i < resultCount; i++){
+				relatedList[] = {
+					"link": body[3],
+					"label": body[1]
+				}
+			}
+		
+			cb(null, {
             // Required: This is the entity object passed into the integration doLookup method
             entity: entityObj,
             // Required: An object containing everything you want passed to the template
@@ -115,8 +125,7 @@ function _lookupEntity(entityObj, options, cb) {
                 details: {
                     para: body[2][0],
 					relatedCount: relatedCount,
-					list: body[1],
-					url: body[3],
+					relatedList: relatedList
                 }
             }
         }); }
