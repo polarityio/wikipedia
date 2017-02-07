6e3b13258a4877b3af9f535836429568f7a8c6e0
rader <joem@breachintelligence.com>
2017-02-07 15:34:09 -0500

    Adding back in regex to remove To a Section:

--- components/block.js
+++ components/block.js
@@ -1,2 +1,13 @@
 polarity.export = PolarityComponent.extend({
+    extract: function(){
+
+        if(typeof(this.get("block.data.details.body[2][0]")) == "undefined"){
+            return "";
+        }
+
+        var e = this.get("block.data.details.body[2][0]");
+        var shortentedText = e.match(/^.{1,340}[^\.]*/)+".";
+
+        return shortentedText;
+    }.property("block.data.details.extract")
 });
