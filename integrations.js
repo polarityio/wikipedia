d6b6ab5d705f5c5c3c9b1d9605bf973b239bfdfc
Ed <ed@polarity.io>
2018-05-16 20:41:24 -0400

    Add support for specifying a proxy

--- integrations.js
+++ integrations.js
@@ -2,16 +2,43 @@
 
 let request = require('request');
 let _ = require('lodash');
+let config = require('./config/config.js');
+let fs = require('fs');
 let async = require('async');
+
 let log = null;
 let entityNoSpecialChars = /^[^#<>\[\]|\{\}\/:]+$/;
 let entityNotGeo = /[\d\s]+,[\d\s]+/;
 let wikiRedirect = /^((To|From)[a-zA-Z ]+:)|([\w\s]+may refer to:)/i;
+let requestWithDefaults;
 
 const VALID_SEARCH_PROFILES = ['strict', 'normal', 'fuzzy', 'classic'];
 
 function startup(logger) {
     log = logger;
+    let defaults = {};
+
+    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+        defaults.cert = fs.readFileSync(config.request.cert);
+    }
+
+    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+        defaults.key = fs.readFileSync(config.request.key);
+    }
+
+    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+        defaults.passphrase = config.request.passphrase;
+    }
+
+    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+        defaults.ca = fs.readFileSync(config.request.ca);
+    }
+
+    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+        defaults.proxy = config.request.proxy;
+    }
+
+    requestWithDefaults = request.defaults(defaults);
 }
 
 function _validateOptions(options) {
@@ -115,7 +142,7 @@ function _lookupEntity(entityObj, options, cb) {
 
     log.debug({uri: uri}, "Checking to see if the query executes");
 
-    request({
+    requestWithDefaults({
         uri: uri,
         method: 'GET',
         json: true
